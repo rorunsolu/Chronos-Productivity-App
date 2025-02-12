b54b8845e10f960ded9268e3be4048f476f9c5f8
Ray Orunsolu <rorunsolu@gmail.com>
2025-02-12 15:36:20 +0000

    mofiied vite config try and fix the redirect with google sign in issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/__/auth": {
+        target: "https://${env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/__\/auth/, "/__/auth"),
+      },
+    },
+  },
+});
