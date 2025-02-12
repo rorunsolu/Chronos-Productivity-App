208becfd8d7a4f15580a187371df5b7af1d06778
Ray Orunsolu <rorunsolu@gmail.com>
2025-02-12 15:38:51 +0000

    authenticated users can navgigate to a dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import SignIn from "./components/auth/login/SignIn";
-import SignUp from "./components/auth/register/SignUp";
+import SignIn from "./pages/login/SignIn";
+import SignUp from "./pages/register/SignUp";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Protected from "./components/auth/Protected";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
+import { AuthContextProvider } from "./contexts/authContext/AuthContext";
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/dashboard" element={
+            <Protected>
+              <Dashboard />
+            </Protected>}
+          />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,24 @@
+import "./Dashboard.css";
+import { UserAuth } from "../../contexts/authContext/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Dashboard = () => {
+    const navigate = useNavigate();
+
+    const { user } = UserAuth();
+
+    useEffect(() => {
+        if (user === null) {
+            navigate("/");
+        }
+    }, [user, navigate]);
+
+    return (
+        <div className="container">
+            <h1 className="dashboard__title">{user?.displayName}'s Dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
