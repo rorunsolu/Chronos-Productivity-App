d7745fdb9b70bd2313ad7fa8c5c512336d130c06
Ray Orunsolu <rorunsolu@gmail.com>
2025-02-15 16:56:13 +0000

    installed taiwlind

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 // export default defineConfig({
@@ -7,7 +8,7 @@ import react from '@vitejs/plugin-react';
 // })
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   server: {
     proxy: {
       "/__/auth": {
