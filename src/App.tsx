f51fde88726a06f9fa9de5eef781139eac5fe621
Ray Orunsolu <rorunsolu@gmail.com>
2025-02-22 01:34:15 +0000

    modfied slightly to allow for use of new contexts for notes and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import SignIn from "./pages/login/SignIn";
-import SignUp from "./pages/register/SignUp";
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Dashboard from "./pages/dashboard/Dashboard";
-import TaskPage from "./pages/tasks/TaskPage";
-import Protected from "./components/auth/Protected";
-import { AuthContextProvider } from "./contexts/authContext/AuthContext";
-import { Routes, Route } from "react-router-dom";
+import Navbar from './components/navbar/Navbar';
+import Protected from './components/auth/Protected';
+import Sidebar from './components/layout/sidebar/Sidebar';
+import { AuthContextProvider } from './contexts/authContext/AuthContext';
+import { NoteProvider } from './features/Notes/context/NoteContext';
+import { TaskProvider } from './features/Tasks/context/TaskContext';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import SignIn from "./pages/Sign In Page/SignInPage";
+import SignUp from "./pages/Sign Up Page/SignUpPage";
+import Home from "./pages/Home Page/HomePage";
+import Dashboard from "./pages/Dashboard Page/DashboardPage";
+import TaskPage from "../src/pages/Tasks Page/TaskPage";
+import NotesPage from "./pages/Notes Page/NotesPage";
+import NoteEditorPage from './pages/Note Editor Page/NoteEditorPage';
 
 function App() {
 
@@ -15,21 +20,36 @@ function App() {
     <>
       <AuthContextProvider>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/dashboard" element={
-            <Protected>
-              <Dashboard />
-            </Protected>}
-          />
-          <Route path="/tasks" element={
-            <Protected>
-              <TaskPage />
-            </Protected>}
-          />
-        </Routes>
+        <Sidebar />
+        <NoteProvider>
+          <TaskProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/dashboard" element={
+                <Protected>
+                  <Dashboard />
+                </Protected>}
+              />
+              <Route path="/tasks" element={
+                <Protected>
+                  <TaskPage />
+                </Protected>}
+              />
+              <Route path="/notes" element={
+                <Protected>
+                  <NotesPage />
+                </Protected>}
+              />
+              <Route path="/notes/:id" element={
+                <Protected>
+                  <NoteEditorPage />
+                </Protected>}
+              />
+            </Routes>
+          </TaskProvider>
+        </NoteProvider>
       </AuthContextProvider>
     </>
   );
