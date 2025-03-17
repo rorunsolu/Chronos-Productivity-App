93cf9507bf575a76a4f22f3f150287263e6bcf80
Ray Orunsolu <rorunsolu@gmail.com>
2025-03-17 21:34:00 +0000

    updated configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/__/auth": {
         target: "https://${env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com",
+        // target: `https://${process.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`, // Ensure env variable is correctly set
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/__\/auth/, "/__/auth"),
       },
