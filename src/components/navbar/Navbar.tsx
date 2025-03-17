1c18fb3a1a4a15f0eaa460edb27ac6c7701596a7
Ray Orunsolu <rorunsolu@gmail.com>
2025-03-17 21:32:59 +0000

    navabr component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,51 +1,59 @@
-import "./Navbar.scss";
+import "@/components/Navbar/Navbar.scss";
 import { Link } from 'react-router-dom';
-import { UserAuth } from '../../contexts/authContext/AuthContext';
+import { UserAuth } from "@/contexts/authContext/AuthContext";
 
 const Navbar = () => {
-    const { user, logOut } = UserAuth();
-
-    const handleSignOut = async () => {
-        try {
-            await logOut();
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    const { user } = UserAuth();
 
     return (
         <>
-            {user ? null : (
-                <nav className="navbar">
+            {user ? <nav className="navbar">
 
-                    <div className="navbar__inner">
+                <div className="navbar__inner">
 
-                        <div className="navbar__content">
+                    <div className="navbar__content">
 
-                            <div className="navbar__logo">
-                                <img className="logo" src="/chronos-logo.svg" alt="Chronos Logo" />
-                                <span className="navbar__brand">Chronos</span>
-                            </div>
-
+                        <div className="navbar__logo">
+                            <img className="logo" src="/chronos-logo.svg" alt="Chronos Logo" />
+                            <span className="navbar__brand">Chronos</span>
                         </div>
-
-                        <div className="navbar__actions">
-
-                            <Link to="/SignIn">
-                                <button className="navbar__actions-btn">Sign in</button>
-                            </Link>
-
-                            <Link to="/SignUp">
-                                <button className="navbar__actions-btn">Sign up</button>
-                            </Link>
-
-                        </div>
-
 
                     </div>
 
-                </nav>
-            )}
+                </div>
+
+            </nav>
+                : (
+                    <nav className="navbar">
+
+                        <div className="navbar__inner">
+
+                            <div className="navbar__content">
+
+                                <div className="navbar__logo">
+                                    <img className="logo" src="/chronos-logo.svg" alt="Chronos Logo" />
+                                    <span className="navbar__brand">Chronos</span>
+                                </div>
+
+                            </div>
+
+                            <div className="navbar__actions">
+
+                                <Link to="/SignIn">
+                                    <button className="navbar__actions-btn">Sign in</button>
+                                </Link>
+
+                                <Link to="/SignUp">
+                                    <button className="navbar__actions-btn">Sign up</button>
+                                </Link>
+
+                            </div>
+
+
+                        </div>
+
+                    </nav>
+                ) }
         </>
     );
 };
