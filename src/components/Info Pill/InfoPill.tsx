b04425706eace91ff27d5886510e9781f6ba7ae5
Ray Orunsolu <rorunsolu@gmail.com>
2025-04-06 18:39:04 +0100

    new component

---
+++ src/components/Info Pill/InfoPill.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import "@/components/Info Pill/InfoPill.scss";
+
+const InfoPill: React.FC<InfoPillProps> = ({ icon, value }) => {
+    return (
+        <div className="info-pill line-clamp-1">
+            { icon }
+            { value }
+        </div>
+    )
+}
+
+export default InfoPill;
+
+interface InfoPillProps {
+    icon: ReactNode;
+    value: string;
+}
