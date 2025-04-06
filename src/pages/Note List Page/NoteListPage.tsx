27ff228f09f73038d6bd73785a3149119deb8426
Ray Orunsolu <rorunsolu@gmail.com>
2025-04-06 19:50:29 +0100

    minor changes

--- src/pages/Note List Page/NoteListPage.tsx
+++ src/pages/Note List Page/NoteListPage.tsx
@@ -1,34 +1,58 @@
-import Modal from "react-modal";
+import Dropdown from "@/components/Dropdown/Dropdown";
+import { UseNotes } from "@/features/Notes/context/NoteContext";
+import { ArrowDownUp, ArrowUpDown, Plus, Search } from "lucide-react";
+import { Tag } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import { ListFilter, Plus, Search, EllipsisVertical } from "lucide-react";
-import { UseNotes } from "../../features/Notes/context/NoteContext";
-import "./NoteListPage.scss";
-import "../../App.css";
+import Modal from "react-modal";
+import TextareaAutosize from "react-textarea-autosize";
+import "@/pages/Note List Page/NoteListPage.scss";
 import NoteListCard from "@/features/Notes/Note List card/NoteListCard";
+import { UserAuth } from "@/contexts/authContext/AuthContext";
 
 const NoteListPage = () => {
 
+   const { user } = UserAuth();
+
    const { notes, fetchNotes, createNote, deleteNote } = UseNotes();
+
    const [isModalOpen, setIsModalOpen] = useState(false);
    const openModal = () => setIsModalOpen(true);
-   const [noteTitle, setNoteTitle] = useState("");
+
    const [noteContent, setNoteContent] = useState("");
+   const [noteFolder, setNoteFolder] = useState("");
+   const [noteLabel, setNoteLabel] = useState("");
    const [searchQuery, setSearchQuery] = useState("");
 
-   const filteredNotes = notes.filter(task => {
-      const searchLower = searchQuery.toLowerCase();
-      return (
-         task.title.toLowerCase().includes(searchLower) ||
-         task.content?.toLowerCase().includes(searchLower)
-      );
-   });
+   const [isNewestFirst, setIsNewestFirst] = useState(true);
+   const labelOptions = ["Personal", "Work", "School"];
 
+   const filteredNotes = notes
+      .filter(note => {
+         const searchLower = searchQuery.toLowerCase();
+         return (
+            note.content?.toLowerCase().includes(searchLower)
+         );
+      })
+      .sort((a, b) => {
+         const dateA = a.createdAt.toDate();
+         const dateB = b.createdAt.toDate();
+         return isNewestFirst
+            ? dateB.getTime() - dateA.getTime()
+            : dateA.getTime() - dateB.getTime();
+      });
 
    const handleCreateNote = async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      createNote(noteTitle, noteContent);
-      setNoteTitle("");
+
+      if (!user) {
+         console.error("User is not authenticated.");
+         return;
+      }
+
+      createNote(noteContent, noteFolder, noteLabel, user.uid);
       setNoteContent("");
+      setNoteFolder("");
+      setNoteLabel("");
       setIsModalOpen(false);
    };
 
@@ -40,7 +64,8 @@ const NoteListPage = () => {
    //todo: Add tooltips for the buttons
 
    return (
-      <div className="flex w-full h-full justify-center items-center mt-16">
+      <div className="page-wrapper">
+
          <div className="note-list-page">
 
             <div className="note-list-page__header">
@@ -61,12 +86,11 @@ const NoteListPage = () => {
                   <Plus />
                </button>
 
-               <button className="note-list-page__actions-button" onClick={ () => console.log("Filter button clicked") }>
-                  <ListFilter />
-               </button>
-
-               <button className="taskpage__actions-button">
-                  <EllipsisVertical />
+               <button
+                  className="note-list-page__actions-button"
+                  onClick={ () => setIsNewestFirst(!isNewestFirst) }
+               >
+                  { isNewestFirst ? (<ArrowDownUp />) : (<ArrowUpDown />) }
                </button>
 
             </div>
@@ -81,40 +105,50 @@ const NoteListPage = () => {
                isOpen={ isModalOpen }
                onRequestClose={ () => setIsModalOpen(false) }
                contentLabel="Create a new note"
-               className="modal"
+               className={ `modal ${isModalOpen ? "modal--open" : ""}` }
                overlayClassName="modal-overlay"
                appElement={ document.getElementById('root') || undefined }>
 
-               <p className="modal__title">Create note</p>
+               <h3 className="modal__title">Create note</h3>
 
                <form className="modal__form" onSubmit={ handleCreateNote }>
 
-                  <input
-                     className="modal__input"
-                     onChange={ (e) => setNoteTitle(e.target.value) }
-                     value={ noteTitle }
-                     type="text"
-                     placeholder="Title"
-                  />
-
-                  <textarea
+                  <TextareaAutosize
                      className="modal__textarea"
                      onChange={ (e) => setNoteContent(e.target.value) }
                      value={ noteContent }
-                     placeholder="Content"
                   />
 
-                  <button className="modal__button" disabled={ !noteTitle || !noteContent } type="submit">
-                     Create note
-                  </button>
+                  <div className="modal__option-wrapper">
 
-                  <button
-                     className="modal__button"
-                     onClick={ () => setIsModalOpen(false) }
-                     type="button"
-                  >
-                     Cancel
-                  </button>
+                     <div className="modal__option">
+                        <label className="modal__label"><Tag size={ 20 } /></label>
+                        <Dropdown
+                           options={ labelOptions }
+                           value={ noteLabel }
+                           onChange={ (value: string) => setNoteLabel(value) }
+                           placeholder="Select a label"
+                        />
+                     </div>
+
+                  </div>
+
+                  <div className="modal__button-wrapper">
+
+                     <button className="modal__button modal__button--create" disabled={ !noteContent } type="submit">
+                        Create note
+                     </button>
+
+                     <button
+                        className="modal__button modal__button--cancel"
+                        onClick={ () => setIsModalOpen(false) }
+                        type="button"
+                     >
+                        Cancel
+                     </button>
+                  </div>
+
+
 
                </form>
 
--- src/pages/Project List Page/ProjectListPage.tsx
+++ src/pages/Project List Page/ProjectListPage.tsx
@@ -1,36 +1,36 @@
 import Dropdown from "@/components/Dropdown/Dropdown";
-import Modal from "react-modal";
-import React, { useEffect, useState } from "react";
+import { UserAuth } from "@/contexts/authContext/AuthContext";
 import { UseProjects } from "@/features/Projects/context/ProjectContext";
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
 import "@/pages/Project List Page/ProjectListPage.scss";
 import ProjectListCard from "@/features/Projects/Project List Card/ProjectListCard";
-import {
-   ArrowDownUp,
-   EllipsisVertical,
-   ListFilter,
-   Plus,
-   Search,
-} from "lucide-react";
-
+import { ArrowDownUp, ArrowUpDown, Search, Plus } from "lucide-react";
 
 const ProjectPage = () => {
+   const { user } = UserAuth();
    const { projects, fetchProjects, createProject, deleteProject } = UseProjects();
    const [isModalOpen, setIsModalOpen] = useState(false);
    const openModal = () => setIsModalOpen(true);
    const [projectTitle, setProjectTitle] = useState("");
    const [projectDescription, setProjectDescription] = useState("");
    const [projectLabel, setProjectLabel] = useState("");
-   const [projectPriority, setProjectPriority] = useState("");
-   const priorityOptions = ["Low", "Medium", "High"];
    const labelOptions = ["Work", "Personal", "Urgent"];
+   const [isNewestFirst, setIsNewestFirst] = useState(true);
+   const [searchQuery, setSearchQuery] = useState("");
 
    const handleCreateProject = async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
-      createProject(projectTitle, projectDescription, projectLabel, projectPriority);
+
+      if (!user) {
+         console.error("User is not authenticated.");
+         return;
+      }
+
+      createProject(projectTitle, projectDescription, projectLabel, user.uid);
       setProjectTitle("");
       setProjectDescription("");
       setProjectLabel("");
-      setProjectPriority("");
 
       setIsModalOpen(false);
    };
@@ -39,11 +39,28 @@ const ProjectPage = () => {
       fetchProjects();
    }, [fetchProjects]);
 
+   const filteredProjects = projects
+      .filter(project => {
+         const searchLower = searchQuery.toLowerCase();
+         return (
+            project.name.toLowerCase().includes(searchLower) ||
+            project.status?.toLowerCase().includes(searchLower) ||
+            project.label?.toLowerCase().includes(searchLower)
+         );
+      })
+      .sort((a, b) => {
+         const dateA = a.createdAt.toDate();
+         const dateB = b.createdAt.toDate();
+         return isNewestFirst
+            ? dateB.getTime() - dateA.getTime()
+            : dateA.getTime() - dateB.getTime();
+      });
+
    return (
-      <div className="flex w-full h-full justify-center items-center mt-16">
+      <div className="page-wrapper">
 
          <div className="project-list-page">
-            
+
             <div className="project-list-page__header">
                <h1 className="project-list-page__title">Projects</h1>
                <p className="project-list-page__subtitle">All your projects in one place</p>
@@ -52,19 +69,18 @@ const ProjectPage = () => {
             <div className="project-list-page__actions">
                <form className="project-list-page__form">
                   <Search />
-                  <input className="project-list-page__form-input" type="text" placeholder="Search projects" />
+                  <input className="project-list-page__form-input" type="text" placeholder="Search projects" onChange={ (e) => setSearchQuery(e.target.value) } />
                </form>
-
-               <button className="project-list-page__button" onClick={ () => console.log("Filter button clicked") }>
-                  <ListFilter />
-               </button>
 
                <button className="project-list-page__button" onClick={ openModal }>
                   <Plus />
                </button>
 
-               <button className="project-list-page__button">
-                  <EllipsisVertical />
+               <button
+                  className="project-list-page__button"
+                  onClick={ () => setIsNewestFirst(!isNewestFirst) }
+               >
+                  { isNewestFirst ? (<ArrowDownUp />) : (<ArrowUpDown />) }
                </button>
 
             </div>
@@ -72,8 +88,8 @@ const ProjectPage = () => {
             <div>
                <ul className="project-list-page__project-list">
 
-                  { projects.map((project) => (
-                     <ProjectListCard project={ project } key={ project.id } />
+                  { filteredProjects.map((project) => (
+                     <ProjectListCard project={ project } key={ project.id } deleteProject={ deleteProject } />
                   )) }
 
                </ul>
@@ -83,9 +99,10 @@ const ProjectPage = () => {
                isOpen={ isModalOpen }
                onRequestClose={ () => setIsModalOpen(false) }
                contentLabel="Create a new project"
-               className="modal"
+               className={ `modal ${isModalOpen ? "modal--open" : ""}` }
                overlayClassName="modal-overlay"
-               appElement={ document.getElementById('root') || undefined }>
+               appElement={ document.getElementById('root') || undefined }
+            >
 
                <p className="modal__title">Create a new project</p>
                <form className="modal__form" onSubmit={ handleCreateProject }>
@@ -106,15 +123,13 @@ const ProjectPage = () => {
 
                   <Dropdown value={ projectLabel } onChange={ setProjectLabel } options={ labelOptions } placeholder="Select a label" />
 
-                  <Dropdown value={ projectPriority } onChange={ setProjectPriority } options={ priorityOptions } placeholder="Select a priority" />
-
                   <div className="modal__button-wrapper">
-                     <button className="modal__button" disabled={ !projectTitle || !projectDescription } type="submit">
+                     <button className="modal__button modal__button--create" disabled={ !projectTitle || !projectDescription } type="submit">
                         Create project
                      </button>
 
                      <button
-                        className="modal__button"
+                        className="modal__button modal__button--cancel"
                         onClick={ () => setIsModalOpen(false) }
                         type="button"
                      >
