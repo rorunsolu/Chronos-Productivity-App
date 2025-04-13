5d4692d8ebbc4471147613a3652cf79799b2ac45
Ray Orunsolu <rorunsolu@gmail.com>
2025-04-13 17:58:23 +0100

    minor changes

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,120 +2,151 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { UserAuth } from "@/contexts/authContext/AuthContext";
 import "@/components/Sidebar/Sidebar.scss";
-import { Layers, FolderOpen, ChartColumnDecreasing, Notebook, SquareCheckBig, Calendar } from 'lucide-react';
+import {
+  Layers,
+  FolderOpen,
+  ChartColumnDecreasing,
+  Notebook,
+  SquareCheckBig,
+  // Calendar,
+} from "lucide-react";
 
 const Sidebar: React.FC<SidebarProps> = ({ className, isSidebarExpanded }) => {
-   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
-   const [activeMenuItem, setActiveMenuItem] = useState("Home");
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+  const [activeMenuItem, setActiveMenuItem] = useState("Home");
 
-   const { user, logOut } = UserAuth();
+  const { user, logOut } = UserAuth();
 
-   const handleSignOut = async () => {
-      try {
-         await logOut();
-      } catch (error) {
-         console.log(error);
-      }
-   };
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-   return (
-
-      <aside
-         className={ `
-                sidebar 
-                ${isSidebarExpanded ? "w-66 transition-all" : "w-16 transition-all"} 
+  return (
+    <aside
+      className={`
+                sidebar  transition-all
+                ${isSidebarExpanded ? "w-66" : "w-16"} 
                 ${user ? "" : "hidden"} 
                 ${className || ""}
             `}
-      >
-         <nav className="sidebar__nav">
+    >
+      <nav className="sidebar__nav">
+        <div
+          className={`sidebar__header ${
+            isSidebarExpanded ? "justify-start" : ""
+          }`}
+        >
+          <img className={`sidebar__logo`} src="chronos-logo.svg" />
+          <p
+            className={`sidebar__logo-text ${
+              isSidebarExpanded ? "" : "hidden"
+            } `}
+          >
+            Chronos
+          </p>
+        </div>
 
-            <div className={ `sidebar__header ${isSidebarExpanded ? "justify-start" : ""}` }>
-               <img className={ `sidebar__logo` } src="chronos-logo.svg" />
-               <p className={ `sidebar__logo-text ${isSidebarExpanded ? "" : "hidden"} ` }>Chronos</p>
-            </div>
-
-            <ul className="sidebar__list">
-               { sidebarItemList.map(({ name, icon }) => (
-                  <Link className="" to={ `/${name}` } key={ name }>
-                     <li
-                        className={ `sidebar-item group
+        <ul className="sidebar__list">
+          {sidebarItemList.map(({ name, icon }) => (
+            <Link className="" to={`/${name}`} key={name}>
+              <li
+                className={`sidebar-item group
                         ${activeMenuItem === name ? "sidebar-item--active" : ""}
-                        ${isSidebarExpanded ? "" : ""}` }
-                        onClick={ () => setActiveMenuItem(name) }
-                     >
-                        <span className={ `sidebar-item__icon ${isSidebarExpanded ? "ml-1.25" : "ml-1.25"}` }>
-                           { icon }
-                        </span>
+                        ${isSidebarExpanded ? "" : ""}`}
+                onClick={() => setActiveMenuItem(name)}
+              >
+                <span
+                  className={`sidebar-item__icon ${
+                    isSidebarExpanded ? "ml-1.25" : "ml-1.25"
+                  }`}
+                >
+                  {icon}
+                </span>
 
-                        <span className={ `sidebar-item__text overflow-hidden capitalize ${isSidebarExpanded ? "" : "hidden"}` }>
-                           { name }
-                        </span>
+                <span
+                  className={`sidebar-item__text overflow-hidden capitalize ${
+                    isSidebarExpanded ? "" : "hidden"
+                  }`}
+                >
+                  {name}
+                </span>
 
-                        { !isSidebarExpanded && (
-                           <div className={ `sidebar-item__tooltip 
-                              invisible opacity-20 -translate-x-3 group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
-                              { name }
-                           </div>
-                        ) }
-                     </li>
-                  </Link>
-               )) }
-            </ul>
-
-            <div className="sidebar__footer" onClick={ () => setIsUserMenuOpen(prevState => !prevState) }>
-
-               <div className="sidebar__user-container">
-
-                  { user?.photoURL ? (
-                     <img className="sidebar__avatar" src={ user.photoURL } />
-                  ) : (
-                     <img className="sidebar__avatar" src="./placeholder.png" />
-                  ) }
-
-                  <div className={ `sidebar__user ${isSidebarExpanded ? "w-full" : "w-0"}` }>
-                     { user?.displayName ? (
-                        <div className="sidebar__user-info">
-                           <h4 className="sidebar__user-name line-clamp-1">{ user.displayName }</h4>
-                           <span className="sidebar__user-email line-clamp-1">{ user.email }</span>
-                        </div>
-                     ) : (
-                        ""
-                     ) }
-
+                {!isSidebarExpanded && (
+                  <div
+                    className={`sidebar-item__tooltip 
+                              invisible opacity-20 -translate-x-3 group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
+                  >
+                    {name}
                   </div>
+                )}
+              </li>
+            </Link>
+          ))}
+        </ul>
 
-                  { isUserMenuOpen && (
-                     <div className="sidebar__user-menu">
+        <div
+          className="sidebar__footer"
+          onClick={() => setIsUserMenuOpen((prevState) => !prevState)}
+        >
+          <div className="sidebar__user-container">
+            {user?.photoURL ? (
+              <img className="sidebar__avatar" src={user.photoURL} />
+            ) : (
+              <img className="sidebar__avatar" src="./placeholder.png" />
+            )}
 
-                        <button className="sidebar__user-button" onClick={ handleSignOut }>
-                           Sign Out
-                        </button>
-
-                     </div>
-                  ) }
-
-               </div>
-
+            <div
+              className={`sidebar__user ${
+                isSidebarExpanded ? "w-full" : "w-0"
+              }`}
+            >
+              {user?.displayName ? (
+                <div className="sidebar__user-info">
+                  <h4 className="sidebar__user-name line-clamp-1">
+                    {user.displayName}
+                  </h4>
+                  <span className="sidebar__user-email line-clamp-1">
+                    {user.email}
+                  </span>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
 
-         </nav>
-      </aside>
-   );
+            {isUserMenuOpen && (
+              <div className="sidebar__user-menu">
+                <button
+                  className="sidebar__user-button"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
 
 const sidebarItemList = [
-   { name: "Dashboard", icon: <ChartColumnDecreasing /> },
-   { name: "Tasks", icon: <SquareCheckBig /> },
-   { name: "Notes", icon: <Notebook /> },
-   { name: "Projects", hasSubMenu: true, icon: <Layers /> },
-   { name: "Folders", icon: <FolderOpen /> },
-   { name: "Calendar", icon: <Calendar /> }
+  { name: "Dashboard", icon: <ChartColumnDecreasing /> },
+  { name: "Tasks", icon: <SquareCheckBig /> },
+  { name: "Notes", icon: <Notebook /> },
+  { name: "Projects", hasSubMenu: true, icon: <Layers /> },
+  { name: "Folders", icon: <FolderOpen /> },
+  // { name: "Calendar", icon: <Calendar /> },
 ];
 
 interface SidebarProps {
-   className?: string;
-   isSidebarExpanded: boolean;
+  className?: string;
+  isSidebarExpanded: boolean;
 }
