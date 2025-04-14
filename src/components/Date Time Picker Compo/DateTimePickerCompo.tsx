129a44038879e269915d7187c2c585901071f1a3
Ray Orunsolu <rorunsolu@gmail.com>
2025-04-14 21:51:34 +0100

    idk

--- src/components/Date Time Picker Compo/DateTimePickerCompo.tsx
+++ src/components/Date Time Picker Compo/DateTimePickerCompo.tsx
@@ -1,30 +1,34 @@
-import { parseISO, format } from 'date-fns';
-import { DatePicker } from '@atlaskit/datetime-picker';
+// import { parseISO, format } from 'date-fns';
+//import { DatePicker } from '@atlaskit/datetime-picker';
+// import { parseISO } from "date-fns";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface DateTimePickerCompoProps {
-	value?: Date | null;
-	onChange: (date: Date | null) => void;
+  selected?: Date | null;
+  onChange: (date: Date | null) => void;
 }
 
-const DateTimePickerCompo = ({ value, onChange }: DateTimePickerCompoProps) => {
-	return (
-		<>
-			<DatePicker
-				dateFormat="YYYY-MM-DD"
-				placeholder="YYYY-MM-DD"
-				parseInputValue={ (date: string) => parseISO(date) }
-				id="datepicker-format"
-				clearControlLabel="Clear due date"
-				shouldShowCalendarButton
-				inputLabelId="dueDate"
-				openCalendarLabel="open calendar"
-				value={ value ? format(value, 'yyyy-MM-dd') : '' }
-				onChange={ (dateString) => {
-					onChange(dateString ? parseISO(dateString) : null);
-				} }
-			/>
-		</>
-	);
+const DateTimePickerCompo = ({
+  selected,
+  onChange,
+}: DateTimePickerCompoProps) => {
+  return (
+    <>
+      <DatePicker
+        //showIcon
+        // dateFormat="yyyy-MM-dd" // Correct format for react-datepicker
+        placeholderText="YYYY-MM-DD" // Correct prop name for placeholder
+        // id="datepicker-format"
+        selected={selected || null} // Pass Date | null directly
+        onChange={(date) => {
+          onChange(date); // Pass Date | null directly to the parent
+        }}
+        // showTimeSelect
+        // dateFormat="Pp"
+      />
+    </>
+  );
 };
 
 export default DateTimePickerCompo;
