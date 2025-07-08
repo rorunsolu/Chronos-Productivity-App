8c004444e51e8a67363dac0d1c571c82670c4b3f
Ray Orunsolu <rorunsolu@gmail.com>
2025-07-08 16:15:30 +0100

    minor style changes + removed unused declarations

--- src/components/Info Pill/InfoPill.tsx
+++ src/components/Info Pill/InfoPill.tsx
@@ -2,17 +2,17 @@ import { ReactNode } from "react";
 import "@/components/Info Pill/InfoPill.scss";
 
 const InfoPill: React.FC<InfoPillProps> = ({ icon, value }) => {
-    return (
-        <div className="info-pill line-clamp-1">
-            { icon }
-            { value }
-        </div>
-    )
-}
+  return (
+    <div className="info-pill line-clamp-1">
+      {icon}
+      {value}
+    </div>
+  );
+};
 
 export default InfoPill;
 
 interface InfoPillProps {
-    icon: ReactNode;
-    value: string;
+  icon: ReactNode;
+  value: string;
 }
