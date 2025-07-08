9b10d2f652f653587bd48b04ca5b9d7430a6a8c8
Ray Orunsolu <rorunsolu@gmail.com>
2025-07-08 16:13:03 +0100

    new btn component

--- src/components/Buttons/ButtonReg.tsx
+++ src/components/Buttons/ButtonReg.tsx
@@ -4,15 +4,22 @@ interface ButtonRegProps {
   label: string;
   onClick?: () => void;
   type?: "primary" | "secondary";
+  formSubmit?: boolean;
 }
 
 const ButtonReg: React.FC<ButtonRegProps> = ({
   label,
   onClick,
   type = "primary",
+  formSubmit = false,
 }) => {
   return (
-    <button className={`button-reg button-reg--${type}`} onClick={onClick}>
+    // Set the button type based on the formSubmit prop
+    <button
+      type={formSubmit ? "submit" : "button"}
+      className={`button-reg button-reg--${type}`}
+      onClick={onClick}
+    >
       {label}
     </button>
   );
